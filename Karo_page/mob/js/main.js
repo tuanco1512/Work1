508e255c73061540dd5ee8b4bbf52bc43bad3823
tuanco1512 <tuanco15122000@gmail.com>
2023-10-20 12:48:56 +0700

    a

---
+++ Karo_page/mob/js/main.js
@@ -0,0 +1,19 @@
+
+$('.btn-menu').click(function(){
+    $(this).toggleClass("click");
+    $('.side-menu').toggleClass("show");
+});
+
+$('.sell-btn').click(function(){
+    $('.sell-list_group').toggleClass("show");
+    $('.side-menu_nav .first').toggleClass("rotate");
+});
+
+$('.rent-btn').click(function(){
+    $('.rent-list_group').toggleClass("show");
+    $('.side-menu_nav .second').toggleClass("rotate");
+});
+
+$('.side-menu_nav li').click(function(){
+    $(this).addClass("active").siblings().removeClass("active");
+});
